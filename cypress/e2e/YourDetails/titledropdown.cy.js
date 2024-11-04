10219f602e3e9dc552ad89772c1d2873854b72c6
Omenebele Eunice Ananenu <omenebele.ea@onthegosystems.com>
2024-11-04 20:45:15 +0000

    Title dropdown tests

--- cypress/e2e/YourDetails/titledropdown.cy.js
+++ cypress/e2e/YourDetails/titledropdown.cy.js
@@ -1,167 +1,114 @@
-describe('visit homepage', () => {
+describe('Visit homepage and test dropdown interactions', () => {
   beforeEach(() => {
     cy.visit('https://www.its4women.ie/');
-    cy.get('#onetrust-accept-btn-handler').click();
-    cy.get('.framer-1y09im0').click();
+    cy.get('#onetrust-accept-btn-handler').click(); // Accept cookies
+    cy.get('.framer-1y09im0').click(); // Interact with necessary page element
   });
 
-//   it('should check the dropdown options', () => {
-//     cy.get('#ctl00_Main_ProposerTitle option')
-//       .should('have.length', 6)
-//       .each(($option, index) => {
-//         const expectedOptions = ['', 'Mrs', 'Miss', 'Ms', 'Mr', 'Dr'];
+  it('should check all dropdown options', () => {
+    const expectedOptions = ['', 'Mrs', 'Miss', 'Ms', 'Mr', 'Dr'];
+    cy.get('#ctl00_Main_ProposerTitle option').should('have.length', 6);
 
-//         // Normalize whitespace and compare text
-//         cy.wrap($option)
-//           .invoke('text')
-//           .then((actualText) => {
-//             const normalizedActualText = actualText.trim().replace(/\s+/g, ' ');
-//             expect(normalizedActualText).to.equal(
-//               expectedOptions[index].trim()
-//             );
-//           });
-//       });
-//   });
+    cy.get('#ctl00_Main_ProposerTitle option').each(($option, index) => {
+      cy.wrap($option)
+        .invoke('text')
+        .then((actualText) => {
+          const normalizedText = actualText.trim().replace(/\s+/g, ' ');
+          expect(normalizedText).to.equal(expectedOptions[index]);
+        });
+    });
+  });
 
-it('should navigate through dropdown options using the select command', () => {
-    
-    // Select 'Mrs' from the dropdown and verify it's selected
-    cy.get('#ctl00_Main_ProposerTitle').select('Mrs').should('have.value', 'Mrs');
-  
-    // Select 'Miss' from the dropdown and verify it's selected
-    cy.get('#ctl00_Main_ProposerTitle').select('Miss').should('have.value', 'Miss');
-  
-    // Select the next option ('Ms') to simulate navigation and verify
+  it('should navigate through dropdown options using the select command', () => {
+    cy.get('#ctl00_Main_ProposerTitle')
+      .select('Mrs')
+      .should('have.value', 'Mrs');
+    cy.get('#ctl00_Main_ProposerTitle')
+      .select('Miss')
+      .should('have.value', 'Miss');
     cy.get('#ctl00_Main_ProposerTitle').select('Ms').should('have.value', 'Ms');
   });
+
+  it('should select "Mrs" when typing "M" in the dropdown', () => {
+    cy.get('#ctl00_Main_ProposerTitle')
+      .select('Mrs')
+      .should('have.value', 'Mrs');
+  });
+
+  it('should select "Dr" when typing "D" in the dropdown', () => {
+    cy.get('#ctl00_Main_ProposerTitle').select('Dr').should('have.value', 'Dr');
+  });
+
+  it('should not select a value when typing "A" in the dropdown', () => {
+    cy.get('#ctl00_Main_ProposerTitle').select(' ').should('have.value', ' ');
+    cy.get('#ctl00_Main_ProposerTitle').type('A');
+    cy.get('#ctl00_Main_ProposerTitle').should('have.value', ' ');
+  });
+
+  it('should not select a value when typing "1" in the dropdown', () => {
+    cy.get('#ctl00_Main_ProposerTitle').select(' ').should('have.value', ' ');
+    cy.get('#ctl00_Main_ProposerTitle').type('1');
+    cy.get('#ctl00_Main_ProposerTitle').should('have.value', ' ');
+  });
+
+  it('should not select a value when typing "!" in the dropdown', () => {
+    cy.get('#ctl00_Main_ProposerTitle').select(' ').should('have.value', ' ');
+    cy.get('#ctl00_Main_ProposerTitle').type('!');
+    cy.get('#ctl00_Main_ProposerTitle').should('have.value', ' ');
+  });
+
+  it('should select "Mrs" when clicked', () => {
+    cy.get('#ctl00_Main_ProposerTitle')
+      .select('Mrs')
+      .should('have.value', 'Mrs');
+  });
+
+  it('should select "Miss" when clicked', () => {
+    cy.get('#ctl00_Main_ProposerTitle')
+      .select('Miss')
+      .should('have.value', 'Miss');
+  });
+
+  it('should select "Ms" when clicked', () => {
+    cy.get('#ctl00_Main_ProposerTitle').select('Ms').should('have.value', 'Ms');
+  });
+
+  it('should select "Mr" when clicked', () => {
+    cy.get('#ctl00_Main_ProposerTitle').select('Mr').should('have.value', 'Mr');
+  });
+
+  it('should select "Dr" when clicked', () => {
+    cy.get('#ctl00_Main_ProposerTitle').select('Dr').should('have.value', 'Dr');
+  });
+
+  it('should show an error message when selecting the blank option', () => {
+    // Select the blank option and verify it's selected
+    cy.get('#ctl00_Main_ProposerTitle').select(' ').should('have.value', ' ');
   
-
-  // it('should select "Mrs" when typing "M" in the dropdown', () => {
-  //   // Click the dropdown to open it
-  //   cy.get('#ctl00_Main_ProposerTitle').click();
-
-  //   // Type "M" into the dropdown
-  //   cy.get('#ctl00_Main_ProposerTitle').type('M');
-
-  //   // Verify that "Mrs" is selected
-  //   cy.get('#ctl00_Main_ProposerTitle').should('have.value', 'Mrs');
-  // });
-
-  // it('should select "Dr" when typing "D" in the dropdown', () => {
-  //   // Click the dropdown to open it
-  //   cy.get('#dropdown-selector').click();
-
-  //   // Type "D" into the dropdown
-  //   cy.get('#dropdown-selector').type('D');
-
-  //   // Verify that "Dr" is selected
-  //   cy.get('#dropdown-selector').should('have.value', 'Dr');
-  // });
-
-  // it('should not select a value when typing "A" in the dropdown', () => {
-  //   // Click the dropdown to open it
-  //   cy.get('#dropdown-selector').click();
-
-  //   // Type "A" into the dropdown
-  //   cy.get('#dropdown-selector').type('A');
-
-  //   // Verify that no value is selected
-  //   cy.get('#dropdown-selector').should('have.value', '');
-  // });
-
-  // it('should not select a value when typing "1" in the dropdown', () => {
-  //   // Click the dropdown to open it
-  //   cy.get('#dropdown-selector').click();
-
-  //   // Type "1" into the dropdown
-  //   cy.get('#dropdown-selector').type('1');
-
-  //   // Verify that no value is selected
-  //   cy.get('#dropdown-selector').should('have.value', '');
-  // });
-
-  // it('should not select a value when typing "!" in the dropdown', () => {
-  //   // Click the dropdown to open it
-  //   cy.get('#dropdown-selector').click();
-
-  //   // Type "!" into the dropdown
-  //   cy.get('#dropdown-selector').type('!');
-
-  //   // Verify that no value is selected
-  //   cy.get('#dropdown-selector').should('have.value', '');
-  // });
-
-  // it('should select "Mrs" when clicked', () => {
-  //   // Click the dropdown to open it
-  //   cy.get('#dropdown-selector').click();
-
-  //   // Click the "Mrs" option
-  //   cy.get('#dropdown-selector option').contains('Mrs').click();
-
-  //   // Verify that "Mrs" is selected
-  //   cy.get('#dropdown-selector').should('have.value', 'Mrs');
-  // });
-
-  // it('should select "Miss" when clicked', () => {
-  //     // Click the dropdown to open it
-  //     cy.get('#dropdown-selector').click();
-
-  //     // Click the "Miss" option
-  //     cy.get('#dropdown-selector option').contains('Miss').click();
-
-  //     // Verify that "Miss" is selected
-  //     cy.get('#dropdown-selector').should('have.value', 'Miss');
-  //   });
-
-  // it('should select "Ms" when clicked', () => {
-  //     // Click the dropdown to open it
-  //     cy.get('#dropdown-selector').click();
-
-  //     // Click the "Ms" option
-  //     cy.get('#dropdown-selector option').contains('Ms').click();
-
-  //     // Verify that "Ms" is selected
-  //     cy.get('#dropdown-selector').should('have.value', 'Ms');
-  //   });
-
-  // it('should select "Mr" when clicked', () => {
-  //     // Click the dropdown to open it
-  //     cy.get('#dropdown-selector').click();
-
-  //     // Click the "Mr" option
-  //     cy.get('#dropdown-selector option').contains('Mr').click();
-
-  //     // Verify that "Mr" is selected
-  //     cy.get('#dropdown-selector').should('have.value', 'Mr');
-  //   });
-
-  // it('should select "Dr" when clicked', () => {
-  //     // Click the dropdown to open it
-  //     cy.get('#dropdown-selector').click();
-
-  //     // Click the "Dr" option
-  //     cy.get('#dropdown-selector option').contains('Dr').click();
-
-  //     // Verify that "Dr" is selected
-  //     cy.get('#dropdown-selector').should('have.value', 'Dr');
-  //   });
-
-  //   it('should show an error message when selecting the blank option', () => {
-  //     // Click the dropdown to open it
-  //     cy.get('#dropdown-selector').click();
-
-  //     // Click the "Blank" option
-  //     cy.get('#dropdown-selector option').first().click();
-
-  //     // Verify the error message (adjust the selector as needed)
-  //     cy.get('.error-message').should('have.text', 'Title Required');
-  //   });
-
-  //   it('should show an error message when submitting without selecting a title', () => {
-  //     // Trigger a form submission (adjust the selector as needed)
-  //     cy.get('form').submit();
-
-  //     // Verify the error message
-  //     cy.get('.error-message').should('have.text', 'Title Required');
-  //   });
+    // Fill out other form fields
+    cy.get('#ctl00_Main_ProposerTelephone').type('07922343359');
+    cy.get('#ctl00_Main_ProposerForename').type('Colin');
+    cy.get('#ctl00_Main_ProposerSurname').type('Darragh');
+    cy.get('#ctl00_Main_ProposerEmail').type('test@dotsys.com');
+  
+    // Submit the form
+    cy.get('#ctl00_Main_Continue1').click();
+  
+    // Verify the error message for the title field
+    cy.get('#ctl00_Main_RF_ProposerTitle').should('have.text', '* Title Required'); // Adjust selector if needed
+  });
+  
+  it('should show an error message when submitting without selecting a title', () => {
+    // Fill out the form without selecting a title
+    cy.get('#ctl00_Main_ProposerTelephone').type('07922343359');
+    cy.get('#ctl00_Main_ProposerForename').type('Colin');
+    cy.get('#ctl00_Main_ProposerSurname').type('Darragh');
+    cy.get('#ctl00_Main_ProposerEmail').type('test@dotsys.com');
+  
+    // Submit the form
+    cy.get('#ctl00_Main_Continue1').click();
+  
+    // Verify the error message for the title field
+    cy.get('#ctl00_Main_RF_ProposerTitle').should('have.text', '* Title Required'); // Adjust selector if needed
+  }); 
 });
