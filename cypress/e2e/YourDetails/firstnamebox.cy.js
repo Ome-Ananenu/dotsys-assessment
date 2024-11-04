0544e8ab4814dcad07682eb6386f16e0aa2e6cc5
Omenebele Eunice Ananenu <omenebele.ea@onthegosystems.com>
2024-11-04 18:46:02 +0000

    First name textbox updates

--- cypress/e2e/YourDetails/firstnamebox.cy.js
+++ cypress/e2e/YourDetails/firstnamebox.cy.js
@@ -25,14 +25,16 @@ describe('visit homepage', () => {
   //   });
 
   it('should display "Forename Required" error on form submission without forename', () => {
-    // Trigger form submission (adjust the selector as needed)
-    cy.get('#ctl00_Main_Continue1')
-      .should('be.visible') // Ensure the element is visible
-      .focus()
-      .type('{enter}');
+    cy.get('#ctl00_Main_ProposerTelephone').type('07922343359{enter}');
+    cy.get('#ctl00_Main_ProposerTitle')
+      .select('Mrs')
+      .should('have.value', 'Mrs');
+    cy.get('#ctl00_Main_ProposerSurname').type('Darragh{enter}');
+    cy.get('#ctl00_Main_ProposerEmail').type('test@dotsys.com{enter}');
+    cy.get('#ctl00_Main_Continue1').click();
 
     // Verify the error message
-    cy.contains('Please enter your first name').should('be.visible');
+    cy.contains('Forename Required').should('be.visible');
   });
 
   it('should display "Please enter your full forename" error on inserting A', () => {
@@ -46,42 +48,55 @@ describe('visit homepage', () => {
   it('should display "Please enter your full forename" error on inserting 1', () => {
     cy.get('#ctl00_Main_ProposerForename').type('1{enter}');
 
-        // Verify the error message
+    // Verify the error message
     cy.contains('Please enter your full forename').should('be.visible');
   });
 
   it('should display "Please enter your full forename" error on inserting a special character', () => {
     cy.get('#ctl00_Main_ProposerForename').type('!{enter}');
-    
-        // Verify the error message
+
+    // Verify the error message
     cy.contains('Please enter your full forename').should('be.visible');
   });
 
-
   it('should allow you proceed with a valid name', () => {
     cy.get('#ctl00_Main_ProposerForename').type('Colin{enter}');
-    
   });
 
   it('should display "Please enter your full forename"a error on inserting an invalid name', () => {
     cy.get('#ctl00_Main_ProposerForename').type('Colin 1{enter}');
-    
+
     cy.contains('Please enter your full forename').should('be.visible');
   });
 
   it('should allow you proceed with a valid name with space', () => {
     cy.get('#ctl00_Main_ProposerForename').type('Colin Christopher{enter}');
-    
+
+    cy.get('#ctl00_Main_ProposerTelephone').type('07922343359{enter}');
+    cy.get('#ctl00_Main_ProposerTitle')
+      .select('Mrs')
+      .should('have.value', 'Mrs');
+    cy.get('#ctl00_Main_ProposerSurname').type('Darragh{enter}');
+    cy.get('#ctl00_Main_ProposerEmail').type('test@dotsys.com{enter}');
+    cy.get('#ctl00_Main_Continue1').click();
+
+    // Verify transition to Your Information page
+    cy.get('#ctl00_Main_UpdatePanel11 > a > .ellipsed > .panel-title')
+      .wait(500) // Add a short delay
+      .should('be.visible')
+      .and('have.text', '2. Your Information');
   });
 
   it('should limit input to 30 characters', () => {
     cy.get('#ctl00_Main_ProposerForename')
-      .type('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNO') // Type 30 characters
-      .should('have.value', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNO') // Verify the input value
+      .type('abcdefghijklmnopqrstuvwxyzABCD') // Type 30 characters
       .wait(500) // Add a short delay
-      .type('P'); // Attempt to type the 31st character
-  
+      .type('E'); // Attempt to type the 31st character
+
     // Assert that the input value remains unchanged (30 characters)
-    cy.get('#ctl00_Main_ProposerForename').should('have.value', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNO');
+    cy.get('#ctl00_Main_ProposerForename').should(
+      'have.value',
+      'abcdefghijklmnopqrstuvwxyzABCD'
+    );
   });
 });
